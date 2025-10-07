2c1eeca7c45c0c18467991969143f3ea4c3503cf
sauravbuilds <sauravbuilds@gmail.com>
2025-10-08 03:17:00 +0530

    Support fixed

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -3,7 +3,7 @@ import { IoMdInformationCircleOutline } from "react-icons/io";
 
 function Support() {
   return (
-    <div className="flex items-center justify-between gap-2 px-4 py-2 rounded-full border bg-zinc-100 border-blue-700 fixed bottom-8 right-10">
+    <div className="hidden lg:flex items-center justify-between gap-2 px-4 py-2 rounded-full border bg-zinc-100 border-blue-700 fixed bottom-8 right-10">
       <span className="text-sm text-blue-700">
         <IoMdInformationCircleOutline />
       </span>
